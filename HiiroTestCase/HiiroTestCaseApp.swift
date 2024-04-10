ec702749f3b100f652a23186f1629902ba97c5f0
Azonaz <malinina.a@gmail.com>
2024-04-10 15:59:15 +0300

    Initial Commit

---
+++ HiiroTestCase/HiiroTestCaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  HiiroTestCaseApp.swift
+//  HiiroTestCase
+//
+//  Created by Админ on 10/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct HiiroTestCaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
